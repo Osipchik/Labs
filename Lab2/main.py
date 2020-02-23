d18cddf955e346f608c45609c25d35c0a5932015
Tim <40692433+Osipchik@users.noreply.github.com>
2020-02-23 19:03:15 +0300

    fix: timer deleted

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,11 +1,9 @@
 import random
 from External_sort import External_sort
 
-# start_time = time.time()
-# with open('C:\\Users\\ASUS\\PycharmProjects\\numbers.txt', 'w') as f:
+# with open('numbers.txt', 'w') as f:
 #     f.writelines('{}\n'.format(random.randint(-1000000, 1000000)) for _ in range(5_000_000))
-# print("--- %s seconds ---" % (time.time() - start_time))
 
 
-# sort = External_sort()
-# sort.sort_file('C:\\Users\\ASUS\\PycharmProjects\\numbers.txt')
+sort = External_sort()
+sort.sort_file('numbers.txt')
